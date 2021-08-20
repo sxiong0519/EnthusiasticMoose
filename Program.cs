f7dc0d00a775753ca9af7d45e14e2bc8d5a13edc
Sue Xiong <sxiong0519@gmail.com>
2021-08-20 14:00:21 -0500

    welcome message

--- Program.cs
+++ Program.cs
@@ -1,12 +1,1 @@
-﻿using System;
-
-namespace EnthusiasticMoose
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+﻿System.Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
