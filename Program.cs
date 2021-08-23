81a5a20c50587f8530fb763ff2753f2aa0e38184
Sue Xiong <sxiong0519@gmail.com>
2021-08-23 10:51:04 -0500

    moose replies

--- Program.cs
+++ Program.cs
@@ -8,9 +8,16 @@ Console.WriteLine();
 MooseSays("H I, I'M  E N T H U S I A S T I C !");
 MooseSays("I really am enthusiastic");
 
-// As a question
+// Ask a question
 bool isTrue = MooseAsks("Is Canada real?");
-Console.WriteLine(isTrue);
+if (isTrue)
+{
+    MooseSays("Really? It seems very unlikely.");
+}
+else
+{
+    MooseSays("I  K N E W  I T !!!");
+}
 
 
 void MooseSays(string message)
